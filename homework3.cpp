20c70b68616efbef010f9b20c5ca19d328deeca4
Hu Zhen <41243126@nfu.edu.tw>
2025-01-07 22:25:51 +0800

    Update homework3.cpp

--- homework3.cpp
+++ homework3.cpp
@@ -130,47 +130,48 @@ public:
         Node* p1 = this->head->link;
         Node* p2 = b.head->link;
 
-        while (p1 != this->head || p2 != b.head) {
-            int c = 0, e = 0;
-            if (p1 == this->head) {
-                c = p2->coef;
-                e = p2->exp;
-                p2 = p2->link;
-            }
-            else if (p2 == b.head) {
-                c = p1->coef;
-                e = p1->exp;
-                p1 = p1->link;
-            }
-            else {
-                if (p1->exp > p2->exp) {
-                    c = p1->coef;
-                    e = p1->exp;
-                    p1 = p1->link;
-                }
-                else if (p1->exp < p2->exp) {
-                    c = p2->coef;
-                    e = p2->exp;
-                    p2 = p2->link;
-                }
-                else {
-                    c = p1->coef + p2->coef;
-                    e = p1->exp;
-                    p1 = p1->link;
-                    p2 = p2->link;
-                }
-            }
-            if (c != 0) {
-                Node* newNode = new Node;
-                newNode->coef = c;
-                newNode->exp = e;
-                newNode->link = result.head;
+        while (p1 != this->head && p2 != b.head) {
+            if (p1->exp < p2->exp) {
+                Node* newNode = new Node{ p1->coef, p1->exp, result.head };
                 tail->link = newNode;
                 tail = newNode;
+                p1 = p1->link;
+            }
+            else if (p1->exp > p2->exp) {
+                Node* newNode = new Node{ p2->coef, p2->exp, result.head };
+                tail->link = newNode;
+                tail = newNode;
+                p2 = p2->link;
+            }
+            else {
+                int newCoef = p1->coef + p2->coef;
+                if (newCoef != 0) {
+                    Node* newNode = new Node{ newCoef, p1->exp, result.head };
+                    tail->link = newNode;
+                    tail = newNode;
+                }
+                p1 = p1->link;
+                p2 = p2->link;
             }
         }
+
+        // 處理剩餘節點
+        while (p1 != this->head) {
+            Node* newNode = new Node{ p1->coef, p1->exp, result.head };
+            tail->link = newNode;
+            tail = newNode;
+            p1 = p1->link;
+        }
+        while (p2 != b.head) {
+            Node* newNode = new Node{ p2->coef, p2->exp, result.head };
+            tail->link = newNode;
+            tail = newNode;
+            p2 = p2->link;
+        }
+
         return result;
     }
+
 
     // 減法運算
     Polynomial operator-(const Polynomial& b) const {
@@ -263,7 +264,7 @@ public:
     }
 
     // 多項式求值
-    float Evaluate(float x) const {
+    float Evaluate(double x) const {
         float sum = 0.0f;
         Node* curr = head->link;
         while (curr != head) {
@@ -312,54 +313,3 @@ int main() {
     cout << "(p1 * p2)(" << x << ") = " << mule.Evaluate(x) << endl;
     return 0;
 }
-/*作業 3
-
-[程式設計專案]
-開發一個 C++ 類別 Polynomial，用來表示和操作具有整數係數的單變數多項式（使用具有頭節點的循環鏈結串列）。每個多項式的每一項將表示為一個節點。因此，這個系統中的節點將有以下三個資料成員：
-
-coef : 係數
-exp : 指數
-link : 指向下一個節點的指標
-每個多項式應表示為具有頭節點的循環鏈結串列。為了有效地刪除多項式，需要使用第 4.5 節中描述的可用空間表（available - space list）和相關函數。單變數多項式的外部（即用於輸入或輸出）表示將假設為由以下形式的整數序列組成：
-n, c₁, e₁, c₂, e₂, c₃, e₃, ..., cₙ, eₙ，其中 eᵢ 代表指數，cᵢ 代表係數，n 表示多項式的項數。指數以遞減順序排列：
-e₁ > e₂ > ... > eₙ。
-
-撰寫並測試以下函數：
-
-istream& operator>>(istream& is, Polynomial& x) :
-    從輸入中讀取多項式並將其轉換為使用頭節點的循環鏈結串列表示。
-
-    ostream& operator<<(ostream& os, Polynomial& x) :
-    將多項式從鏈結串列表示轉換為其外部表示並輸出。
-
-    Polynomial::Polynomial(const Polynomial& a) :
-    [拷貝建構子]
-    將多項式 a 初始化到多項式* this。
-
-    const Polynomial& Polynomial::operator=(const Polynomial& a) const :
-    [賦值運算子]
-    將多項式 a 賦值給多項式* this。
-
-    Polynomial::~Polynomial() :
-    [解構子]
-    將多項式* this 的所有節點返回到可用空間表。
-
-    Polynomial operator+(const Polynomial& b) :
-    [加法]
-    創建並返回多項式* this + b。
-
-    Polynomial operator-(const Polynomial & b) :
-    [減法]
-    創建並返回多項式 * this - b。
-
-    Polynomial operator(const Polynomial & b) : *
-    [乘法]
-    創建並返回多項式 * this * b。
-
-    Polynomial::Evaluate(float x) const :
-    [多項式求值]
-    計算並返回多項式 * this 在 x 處的值。*/
-
-
-
-
